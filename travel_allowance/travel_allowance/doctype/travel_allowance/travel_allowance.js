a1686d8511879f6df7fcac5b98a127af5006444b
Apeksha Raut <apeksharaut52@gmail.com>
2023-12-01 10:49:18 +0530

    “commit”

--- travel_allowance/travel_allowance/doctype/travel_allowance/travel_allowance.js
+++ travel_allowance/travel_allowance/doctype/travel_allowance/travel_allowance.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2023, Apeksha and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Travel Allowance", {
-// 	refresh(frm) {
+frappe.ui.form.on("Travel Allowance", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+});
+
+
+
