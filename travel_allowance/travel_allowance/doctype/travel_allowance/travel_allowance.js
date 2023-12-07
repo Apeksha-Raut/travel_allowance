944c72552094a20b4ec1f81ec979dfa0e6600ebe
Apeksha Raut <apeksharaut52@gmail.com>
2023-12-07 10:30:37 +0530

    6-12-2023

--- travel_allowance/travel_allowance/doctype/travel_allowance/travel_allowance.js
+++ travel_allowance/travel_allowance/doctype/travel_allowance/travel_allowance.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2023, Apeksha and contributors
 // For license information, please see license.txt
 
+
 frappe.ui.form.on("Travel Allowance", {
 	to_location:function(frm){
 		//frappe.msgprint("Welcome");
@@ -11,11 +12,34 @@ frappe.ui.form.on("Travel Allowance", {
 		let cityClass= getCityClass(destination);
 		console.log(cityClass);
 		frm.set_value("class_city", cityClass);
-		
+		frm.refresh_field('class_city');
 
+	},
+	halting_lodging_select:function(frm){
+
+        let halt_lodg=frm.doc.halting_lodging_select;
+        console.log(halt_lodg);
+		
+		if(halt_lodg=="Halting")
+		{
+			frm.toggle_display('lodging',false);
+			frm.toggle_display('halting',true);
+			
+		}
+		else if(halt_lodg==="Lodging"){
+			frm.toggle_display('halting',false);
+			frm.toggle_display('lodging',true);
+		}
+		else{
+			frm.toggle_display('lodging',false);
+		    frm.toggle_display('halting',false);
+		}
 	}
 
+
+
 });
+
 
 // Function to determine the city class based on the destination
 function getCityClass(lowerDestination) {
